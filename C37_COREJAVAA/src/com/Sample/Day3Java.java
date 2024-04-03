376ca426c41a2dbc2e601a46b1a8c7412d4990e0
Imayavarman M <Imayavarman M@DESKTOP-018EIVD>
2024-04-04 02:38:02 +0530

    Day 3 Java

---
+++ C37_COREJAVAA/src/com/Sample/Day3Java.java
@@ -0,0 +1,54 @@
+package com.Sample;
+
+public class Day3Java {
+	private static final String Runs = null;
+	private String name;
+	private int age;
+	private int runs;
+
+	public String getname() {
+		return name;
+
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+
+	}
+
+	public int getRuns() {
+		return runs;
+	}
+
+	public void setRuns(int runs) {
+		this.runs = runs;
+		
+	}
+	
+	public String toString() {
+		return "CSK[Name="+name+", Age="+age+", Runs="+runs+"]";
+	}
+
+	
+	public static void main(String[] args) {
+		ComSample obj= new ComSample();
+		obj.setName("Dhoni");
+		obj.setAge(20);
+		obj.setRuns(50);
+		
+		System.out.println(obj);
+	}
+		
+
+	}
+
+
